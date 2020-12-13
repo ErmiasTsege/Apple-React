e7e8b0d42d95203c7cd0cef348221c232b3bea43
ErmiasTsege <71066745+ErmiasTsege@users.noreply.github.com>
2020-12-12 23:52:45 -0600

    Apple_React First Commit

---
+++ src/Pages/Ipad/ipad.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function ipad() {
+    return (
+        <div>
+        <section className="internal-page-wrapper top-100 bottom-100">
+          <div className="container">
+            <div className="row h-100 align-items-center justify-content-center text-center">
+              <div className="col-12">
+                <h1>My Ipad Page</h1>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+}
+
+export default ipad
